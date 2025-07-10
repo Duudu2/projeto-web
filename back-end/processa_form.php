eb5900b14e322830e248a53136babb1f306a5d0d
Eduardo Mateus <eduardo.santos1081@edu.udesc.br>
2025-07-09 22:07:30 -0300

    Processamento do formulário de inscrição - Final

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -1,11 +1,11 @@
 <?php
     // print_r($_GET);
-    $nome = $_GET['nome'];
+    $name = $_GET['name'];
     $idade = $_GET['idade'];
 
     echo '<h1>Dados do Usuário</h1>';
     echo '<hr>';
-    echo "<p>O usuário $nome tem $idade anos.</p>";
+    echo "<p>O usuário $name tem $idade anos.</p>";
 
     echo '<a href="http://localhost/projeto-web/index.html"><button>Home Page</button></a>';
     echo '<button type="button" onclick="javascript:history.back()">Voltar</button>';
